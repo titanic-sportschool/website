eda72dbb54fdb2ffb1725dd1ea7d365e4cdab26c
bartvs98 <bartvanstraaten98@gmail.com>
2017-01-23 10:28:44 +0100

    Implemented default framework

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
 
+  <head>
+    <title>Team Titanic ¯\_(ツ)_/¯</title>
+    <link rel="icon" href="favicon.ico" type="image/gif">
+    <link rel="stylesheet" type="text/css" href="CSS/global.css">
+    <link rel="stylesheet" type="text/css" href="CSS/global960px.css">
+    <link rel="stylesheet" type="text/css" href="CSS/global540px.css">
+    <link rel="stylesheet" type="text/css" href="CSS/global320px.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  </head>
+
+  <body>
+    <div class="">
+      <?php include("navigation.php") ?>
+    </div>
   </body>
+
 </html>
