2e57c42b0c9d99ded39d2dca648d9c860b957c9a
bartvs98 <bartvanstraaten98@gmail.com>
2017-01-23 17:11:48 +0100

    Added google material font stylesheet

--- Mail.php
+++ Mail.php
@@ -8,16 +8,16 @@
   <div class="home">
 
     <form action="" method="POST">
-      <input type="text" placeholder="Subject" name="subject" autofocus>
+      <input type="text" placeholder="Subject" name="subject" autofocus></span><span class="bar"></span>
       <textarea type="text" placeholder="Message" name="mailvalue" class="mail"></textarea>
     </form>
 
-    <form class="ButtonRight" action="index.php?content=SendMail" method="POST">
-      <button type="submit" name="send">Send</button>
+    <form class="ButtonRight" action="index.php?content=Homepage" method="POST">
+      <button type="submit" name="send"><span>Send </span><i class="material-icons">send</i></button>
     </form>
 
     <form class="ButtonLeft" action="" method="POST">
-      <button onclick="history.go(-1);">Back</button>
+      <button onclick="history.go(-1);return true;">Back</button>
     </form>
 
   </div>
--- index.php
+++ index.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" type="text/css" href="CSS/global960px.css">
     <link rel="stylesheet" type="text/css" href="CSS/global540px.css">
     <link rel="stylesheet" type="text/css" href="CSS/global320px.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   </head>
