70da78d235e44ba1570ba12a3958f9ae96b23751
bartvs98 <bartvanstraaten98@gmail.com>
2017-01-31 13:41:12 +0100

    Added advice page and style/general style improvements

--- Advice.php
+++ Advice.php
@@ -1,5 +1,45 @@
-<?php
+<div class="Home_div">
+  <div class="Log_div">
+    <?php
 
-  
+      include('DB.php');
 
-?>
+      $sql = "SELECT AVG(Calories), Name
+              FROM Equipment_used, Equipment
+              WHERE Equipment.ID = Equipment_used.Equipment_ID
+              GROUP BY Equipment_ID";
+
+      $sqluser = "SELECT AVG(Calories), Name
+                  FROM Equipment_used, Equipment
+                  WHERE Equipment.ID = Equipment_used.Equipment_ID
+                  AND '$_SESSION[User_ID]' = User_id
+                  GROUP BY Equipment_ID";
+
+      $result = mysqli_query($db, $sql);
+      $resultuser = mysqli_query($db, $sqluser);
+
+      while($rowuser = mysqli_fetch_array($resultuser) and $row = mysqli_fetch_array($result)){
+        echo 'Uw <b>' . $rowuser['Name'] . '</b> gemiddelde is <b>' . round($rowuser['AVG(Calories)']) . ' Calorieën</b>' . '<br>';
+        echo 'Het <b>' . $row['Name'] . '</b> gemiddelde van de sportschool is <b>' . round($row['AVG(Calories)']) . ' Calorieën</b>' . '<br><br>';
+        if($row['AVG(Calories)'] > $rowuser['AVG(Calories)']){
+          echo ' Het sportschool gemiddelde ligt hoger dan uw gemiddelde, Het is aan te raden hier nog een tandje bij te zetten.<br><hr><br>';
+        } elseif($row['AVG(Calories)'] == $rowuser['AVG(Calories)']){
+          echo '  U loopt op dit onderdeel gelijk met de rest van de sportschool, ga zo door!<br><hr><br>';
+        } elseif($rowuser['AVG(Calories)'] < $row['AVG(Calories)']){
+          echo '  U loopt op dit onderdeel voor op de rest van de sport school, goed bezig!<br><hr><br>';
+        }
+      }
+
+      echo '<br>';
+
+      while($row = mysqli_fetch_array($result)){
+
+
+      }
+
+    ?>
+    <div class="Button_left">
+      <button onClick="location.href='index.php?content=Homepage'">Terug</button>
+    </div>
+  </div>
+</div>
--- Link.php
+++ Link.php
@@ -9,19 +9,19 @@
       case $customer:
         echo'
 
-          <a href="index.php?content=Profile"><li>My profile</li></a>
-          <a href="index.php?content=Activity"><li>Activity</li></a>
-          <a href="index.php?content=Advice"><li>Advice</li></a>
-          <a href="index.php?content=Mail"><li>Contact staff</li></a>';
+          <a href="index.php?content=Profile"><li>Mijn profiel</li></a>
+          <a href="index.php?content=Activity"><li>Activiteit</li></a>
+          <a href="index.php?content=Advice"><li>Advies</li></a>
+          <a href="index.php?content=Mail"><li>Contacteer medewerker</li></a>';
 
         break;
 
       case $opperator:
         echo'
 
-          <a href="index.php?content=AllCustomers"><li>All customers</li></a>
-          <a href="index.php?content=CustomerLog"><li>Customer log</li></a>
-          <a href="index.php?content=CustomerAdd"><li>Add new user</li></a>';
+          <a href="index.php?content=AllCustomers"><li>Alle leden</li></a>
+          <a href="index.php?content=CustomerLog"><li>Leden logboek</li></a>
+          <a href="index.php?content=CustomerAdd"><li>Nieuwe gebruiker toevoegen</li></a>';
 
         break;
         default:
